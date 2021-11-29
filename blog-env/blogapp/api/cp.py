749142a29d02e6da4a46a7bb9839638f1a1dabe0
umashankargupta <umashankargupta7@gmail.com>
2021-11-29 13:04:59 +0530

    updates

---
+++ blog-env/blogapp/api/cp.py
@@ -0,0 +1,20 @@
+from rest_framework import permissions 
+
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `owner`.
+        return obj.author == request.user
+        
+        
+class IsOwner(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        # Instance must have an attribute named `owner`.
+        return obj == request.user
